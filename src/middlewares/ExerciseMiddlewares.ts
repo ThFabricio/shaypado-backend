63f12d8511628692fc29cffba6e526441fce818c
Thiago Fabricio <thiago.fabricio123@gmail.com>
2024-03-02 01:21:19 -0300

    fix: correcao no middleaware de exercicio

--- src/middlewares/ExerciseMiddlewares.ts
+++ src/middlewares/ExerciseMiddlewares.ts
@@ -14,7 +14,7 @@ export class ExerciseMiddlewares {
             repetitions: Joi.number().integer().min(1).required(),
             calories: Joi.number().min(0).required(),
             user_id: Joi.string().trim().required(),
-            workout_type_id: Joi.array().items(Joi.string().trim()).required(),
+            workoutType: Joi.array().items(Joi.string().trim()).required(),
         });
 
         const { error } = schema.validate(req.body);
